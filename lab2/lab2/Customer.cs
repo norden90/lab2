c43ad37fbff15b2dcb2d150454bd08885c97a605
norden90 <nordfeldt.andreas@gmail.com>
2022-10-03 10:46:04 +0200

    asd

--- lab2/lab2/Customer.cs
+++ lab2/lab2/Customer.cs
@@ -8,15 +8,16 @@ public class Customer
     
     public string Name { get; private set; }
 
-    private string Password { get; set }
+    private string Password { get; set; }
 
     private List<Product> _cart;
     public List<Product> Cart { get { return _cart; } }
 
-    public Kund(string name, string password)
+    public Customer(string name, string password)
     {
         Name = name;
         Password = password;
         _cart = new List<Product>();
+        
     }
 }
--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿//Skapa en konsollaplikation som skall agera som en enkel affär.
+
+//När programmet körs så ska man få se någon form av meny där man ska kunna välja att registrera ny kund eller logga in.
+
+//Därefter ska ytterligare en meny visas där man ska kunna välja att handla, se kundvagn eller gå till kassan.
+
+
+using lab2;
+
+List<Customer> Customers = new List<Customer>();
+
+Customers.Add(new Customer("Knatte", "123"));
+var kund2 = new Customer("Fnatte", "321");
+var kund3 = new Customer("Tjatte", "213");
+
+
+
+Console.WriteLine("Skapa en ny kund");
+
+Console.WriteLine("Ange ditt namn");
+var name = Console.ReadLine();
+Console.WriteLine("Ange ett lösenord");
+var password = Console.ReadLine();
+
+var kund4 = new Customer(name, password);
+
+foreach (var v in Customers)
+{
+    v.Name;
+}
